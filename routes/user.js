5e7699822fc0cd5f4ca35da2069bb65a54f57697
MurtazaShikari <114511800+MurtazaShikari@users.noreply.github.com>
2023-07-08 10:43:44 +0530

    added logout route

--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,10 @@ import {
   getAllUsers,
   getMyProfile,
   login,
+  logout,
   register,
 } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -13,8 +15,9 @@ router.get("/all", getAllUsers);
 
 router.post("/new", register);
 router.post("/login", login);
+router.get("/logout", logout);
 
 //Dynamic Routes should always be in the last
-router.get("/me", getMyProfile);
+router.get("/me", isAuthenticated, getMyProfile);
 
 export default router;
